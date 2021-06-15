1fad914f2ccf6c94396cffc65ba89a7e967ef807
Paul Armer <paul.armer@autodesk.com>
2021-06-14 21:55:07 -0700

    list-block

--- src/character-sheet/sheet-block/list-block.js
+++ src/character-sheet/sheet-block/list-block.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+
+const Label = styled.label`
+	display: flex;
+	align-items: flex-start;
+
+	p {
+		margin-block-start: 0;
+    margin-block-end: 0;
+	}
+`
 
 const ListBlock = ({ name, pick, items }) => {
 	return items.map((item, index) => {
 		const key = `${name} ${index}`
 		return (
-			<label key={key} for={key}>
+			<Label key={key} for={key}>
 				<input id={key} name={key} type="checkbox" />
 				<ReactMarkdown children={item} />
-			</label>
+			</Label>
 		)
 	})
 }
