4f14e025febb9ab129afb77adc4bdc8d078ffb2b
Paul Armer <paul.armer@autodesk.com>
2021-06-15 18:00:10 -0700

    boxes improvements

--- src/character-sheet/sheet-block/boxes-block.js
+++ src/character-sheet/sheet-block/boxes-block.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
 const BoxesBlock = ({ name, count, labels }) => {
+	const [currentLevel, setCurrentLevel] = React.useState(0)
 	const [leftLabel, rightLabel] = labels || []
-	const indexedArray = Array.from(Array(count), (_, i) => i)
+	const indexedArray = Array.from(Array(count), (_, i) => i + 1)
+
+	const setLevel = level => {
+		const newLevel = currentLevel === level ? 0 : level
+		setCurrentLevel(newLevel)
+	}
 
 	return [
 		<span key={`${name} left`}>{leftLabel}</span>,
-		...indexedArray.map(item => {
-			const key = `${name} ${item}`
+		...indexedArray.map(level => {
+			const key = `${name} ${level}`
 			return (
-				<input key={key} name={key} type="checkbox" />
+				<input
+					id={key}
+					key={key}
+					name={name}
+					type="checkbox"
+					checked={level <= currentLevel}
+					onChange={() => setLevel(level)}
+				/>
 			)
 		}),
 		<span key={`${name} right`}>{rightLabel}</span>
