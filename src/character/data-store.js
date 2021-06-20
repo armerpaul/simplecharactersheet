c6339b273eb0fe5736532a561aa4605b17885457
Paul Armer <paul.armer@autodesk.com>
2021-06-20 00:57:36 -0700

    templates!

--- src/character/data-store.js
+++ src/character/data-store.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import * as R from 'ramda'
 
 const lsKey = characterId => `scs-${characterId}`
 const generateCharacterId = () => `${(new Date()).getTime()}`
@@ -39,16 +40,71 @@ export const getCharacterData = ({ characterId }) => {
 	return JSON.parse(raw)
 }
 
+const WILDCARD_SYMBOL = '*'
+const hydrateBlock = ({ templateBlock, sheetBlocks, sheetTemplateBlocks }) => {
+	if (Array.isArray(templateBlock)) {
+		return templateBlock.map(
+			b => hydrateBlock({ templateBlock: b, sheetBlocks, sheetTemplateBlocks })
+		).filter(R.identity)
+	}
+
+	const type = R.prop('type', templateBlock)
+	switch (type) {
+		case 'parent':
+			return hydrateBlock({
+				templateBlock: R.prop('children', templateBlock),
+				sheetBlocks,
+				sheetTemplateBlocks
+			})
+		case 'blocks':
+			return sheetBlocks
+		default:
+			const { name, optional } = templateBlock
+			let fullyHydrated = true
+			const hydratedBlock = R.mapObjIndexed(
+				(value, key) => {
+					if (WILDCARD_SYMBOL === value) {
+						const blockValue = R.path([name, key], sheetTemplateBlocks)
+						if (!blockValue) {
+							if (!optional) {
+								throw new Error(`No sheet value for template @ ${name}.${key}`)
+							}
+							fullyHydrated = false
+							return undefined
+						}
+						return blockValue
+					}
+					else if ('name' === key) {
+						return R.path([name, key], sheetTemplateBlocks) || value
+					}
+
+					return value
+				},
+				templateBlock
+			)
+
+			return fullyHydrated ? hydratedBlock : undefined
+	}
+}
+
 export const getCharacterAndGameData = async ({ gameId, characterId }) => {
 	try {
 		const character = getCharacterData({ characterId })
 		const game = await getGameData({ gameId })
 		const sheet = await getSheetData({ gameId, sheetId: character.sheetId })
+		const blocks = hydrateBlock({
+			templateBlock: R.prop('sheetTemplate', game),
+			sheetBlocks: R.prop('blocks', sheet),
+			sheetTemplateBlocks: R.propOr({}, 'templateBlocks', sheet),
+		})
 
 		return {
 			character,
 			game,
-			sheet,
+			sheet: {
+				...sheet,
+				blocks,
+			},
 		}
 	} catch (error) {
 		console.error(error)
